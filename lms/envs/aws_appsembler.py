2517a6cbb9d0c53f2245a85ae9114e55de0972bf
Filip Jukić <filip@jukic.me>
2016-05-17 16:29:53 +0200

    reorganization of appsembler apps

---
+++ lms/envs/aws_appsembler.py
@@ -0,0 +1,16 @@
+# aws_appsembler.py
+
+from .aws import *
+from .appsembler import *
+
+INSTALLED_APPS += (
+    'appsembler.intercom_integration',
+    'appsembler.enrollment',
+)
+TEMPLATE_CONTEXT_PROCESSORS += ('appsembler.context_processors.intercom',)
+
+SEARCH_SKIP_ENROLLMENT_START_DATE_FILTERING = True
+
+#enable course visibility feature flags
+COURSE_CATALOG_VISIBILITY_PERMISSION = 'see_in_catalog'
+COURSE_ABOUT_VISIBILITY_PERMISSION = 'see_about_page'
