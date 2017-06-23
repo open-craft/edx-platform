feb2cbfc29078af77c073ab4f8c16839a43f0fc9
Sven Marnach <sven@marnach.net>
2017-06-23 13:45:46 +0200

    Add flag to configure whether "Notify users by email" is checked by default.

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -371,6 +371,10 @@ FEATURES = {
 
     # Allow users to change their email address.
     'ALLOW_EMAIL_ADDRESS_CHANGE': True,
+
+    # Whether to check the "Notify users by email" checkbox in the batch enrollment form
+    # in the instructor dashboard.
+    'BATCH_ENROLLMENT_NOTIFY_USERS_DEFAULT': True,
 }
 
 # Ignore static asset files on import which match this pattern
