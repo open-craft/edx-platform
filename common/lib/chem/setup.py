5488ec704a170a3e7e3940981a33b52425f15a48
Daniel Clemente Laboreo <daniel@opencraft.com>
2017-12-06 16:53:06 +0200

    Disable nltk 2.0.6 requirement because it disappeared from PyPi

--- common/lib/chem/setup.py
+++ common/lib/chem/setup.py
@@ -8,6 +8,10 @@ setup(
         "pyparsing==2.0.7",
         "numpy==1.6.2",
         "scipy==0.14.0",
-        "nltk==2.0.6",
+        # Temporarily we comment this dependency, because it's not found in PyPi anymore, and we cannot
+        # get it from an external repository.
+        # nltk==2.0.6 is already required by requirements/edx/github.txt, that means that it will still
+        # work as long as we run 'chem' after installing edx requirements.
+        #"nltk==2.0.6",
     ],
 )
