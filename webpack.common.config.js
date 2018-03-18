b4e68e843b4126841c14c49cd5dd4fad06e5ffb0
Kshitij Sobti <kshitij@sobti.in>
2018-03-19 04:50:56 +0530

    Add UI to browse and select a problem, unit, section etc.

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -30,6 +30,7 @@ module.exports = {
         UpsellExperimentModal: './lms/static/common/js/components/UpsellExperimentModal.jsx',
         PortfolioExperimentUpsellModal: './lms/static/common/js/components/PortfolioExperimentUpsellModal.jsx',
         EntitlementSupportPage: './lms/djangoapps/support/static/support/jsx/entitlements/index.jsx',
+        ProblemBrowser: './lms/djangoapps/instructor/static/instructor/ProblemBrowser/index.jsx',
 
         // Learner Dashboard
         EntitlementFactory: './lms/static/js/learner_dashboard/course_entitlement_factory.js',
