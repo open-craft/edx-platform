8e8a7939a9845001e1233734c45fd3b6d961f92a
Kshitij Sobti <kshitij@sobti.in>
2018-07-18 16:40:20 +0530

    Fix csvUrl parsing
    (cherry picked from commit 4cfc2146ce190551982c2c48e6b4693c39682816)

--- lms/static/js/csv_viewer/data/store.js
+++ lms/static/js/csv_viewer/data/store.js
@@ -5,11 +5,25 @@ import rootReducer from './reducers/index';
 import { fetchCSVData } from './actions/csvFetcher';
 
 const store = createStore(
-    rootReducer,
-    applyMiddleware(thunkMiddleware),
+  rootReducer,
+  applyMiddleware(thunkMiddleware),
 );
 
-const csvUrl = new URLSearchParams(window.location.search).get('csvUrl');
+// We need to use this custom function instead of URLSearchParams since
+// we need to use decodeURI for the csvUrl instead of decodeURIComponent
+function getCsvUrl() {
+  const query = window.location.search.substring(1);
+  const vars = query.split('&');
+  for (let i = 0; i < vars.length; i += 1) {
+    const pair = vars[i].split('=');
+    if (pair[0] === 'csvUrl') {
+      return decodeURI(pair[1]);
+    }
+  }
+  return null;
+}
+
+const csvUrl = getCsvUrl();
 
 store.dispatch(fetchCSVData(csvUrl));
 
