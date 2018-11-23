cc770064b2f627c82aff481208033e9aeb2442c0
Jillian Vogel <jill@opencraft.com>
2018-11-23 17:49:43 +1030

    Move descriptor mixins (TODO with Studio work) back to VideoDescriptor class

--- openedx/core/lib/xblock_builtin/xblock_video/xblock_video/video_module.py
+++ openedx/core/lib/xblock_builtin/xblock_video/xblock_video/video_module.py
@@ -384,7 +384,7 @@ class VideoMixin(object):
         return self.runtime.render_template('video.html', context)
 
 
-class VideoDescriptor(object):
+class VideoDescriptor(TabsEditingDescriptor, EmptyDataRawDescriptor):
     """
     Descriptor for `VideoModule`.
     """
