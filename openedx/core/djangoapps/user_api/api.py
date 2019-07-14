624a8fe58ceb305ef8c23ec192d0ff0b86a3ba7b
pkulkark <pooja@opencraft.com>
2019-07-14 12:40:54 +0530

    Remove read-only override in login form

--- openedx/core/djangoapps/user_api/api.py
+++ openedx/core/djangoapps/user_api/api.py
@@ -211,17 +211,6 @@ def _apply_third_party_auth_overrides(request, form_desc):
                     }
                 )
 
-            if current_provider.other_settings:
-                # Provider config values are parsed as string. Convert into dictionary.
-                other_settings = json.loads(current_provider.other_settings)
-
-                if 'PROVIDER_READ_ONLY_FIELDS' in other_settings:
-                    for field in other_settings['PROVIDER_READ_ONLY_FIELDS']:
-                        form_desc.override_field_properties(
-                            field,
-                            restrictions={"readonly": "readonly"}
-                    )
-
 
 class RegistrationFormFactory(object):
     """HTTP end-points for creating a new user. """
