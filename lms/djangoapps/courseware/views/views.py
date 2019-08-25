93772012d6afc07c74b9b785b78f5151682f1e77
Giovanni Cimolin da Silva <giovannicimolin@gmail.com>
2019-08-25 19:22:27 -0300

    Allow redirection to any course page from course home

--- lms/djangoapps/courseware/views/views.py
+++ lms/djangoapps/courseware/views/views.py
@@ -452,18 +452,22 @@ def course_root(request, course_id):
     Renders the configured course home or redirects to the correct url. This
     behaviour can be controlled by the following variables:
 
+    * DISABLE_UNIFIED_COURSE_TAB_FLAG WaffleFlag
+        redirects / to the unified course experience.
     * COURSE_HOME_BEHAVIOUR SiteConfiguration:
         can redirect from / to any course related page. Some possible redirects
         are: 'info', 'about' and 'courseware'.
         If this fails to find a reverse, it uses the course info page as a
         default.
-    * DISABLE_UNIFIED_COURSE_TAB_FLAG WaffleFlag
-        redirects / to the unified course experience.
 
     If none of those are set, this redirects to course_info (the previous
     default behaviour).
     """
     course_key = CourseKey.from_string(course_id)
+
+    # If the unified course experience is enabled, redirect to the "Course" tab
+    if not DISABLE_UNIFIED_COURSE_TAB_FLAG.is_enabled(course_key):
+        return redirect(reverse(course_home_url_name(course_key), args=[course_id]))
 
     course_home_behaviour = configuration_helpers.get_value('COURSE_HOME_BEHAVIOUR')
     if course_home_behaviour:
@@ -475,10 +479,6 @@ def course_root(request, course_id):
                 'COURSE_HOME_BEHAVIOUR incorrectly configured. The request url doesn\'t exist: %s',
                 course_home_behaviour
             )
-
-    # If the unified course experience is enabled, redirect to the "Course" tab
-    if not DISABLE_UNIFIED_COURSE_TAB_FLAG.is_enabled(course_key):
-        return redirect(reverse(course_home_url_name(course_key), args=[course_id]))
 
     # Default behaviour if variable isn't set
     return course_info(request, course_id)
@@ -520,10 +520,6 @@ def course_info(request, course_id):
         return None
 
     course_key = CourseKey.from_string(course_id)
-
-    # If the unified course experience is enabled, redirect to the "Course" tab
-    if not DISABLE_UNIFIED_COURSE_TAB_FLAG.is_enabled(course_key):
-        return redirect(reverse(course_home_url_name(course_key), args=[course_id]))
 
     with modulestore().bulk_operations(course_key):
         course = get_course_with_access(request.user, 'load', course_key)
