da6659bbbbba879bc90af9f953afed6377b37344
rusrushal13 <rusrushal13@gmail.com>
2020-05-29 21:05:07 +0530

    Fixed the jquery deprecated code.

--- common/lib/xmodule/xmodule/assets/word_cloud/src/js/word_cloud.js
+++ common/lib/xmodule/xmodule/assets/word_cloud/src/js/word_cloud.js
@@ -1,5 +1,3 @@
-(function (require) {
-    require(['xmodule/assets/word_cloud/src/js/word_cloud_main'], function(WordCloudMain) {
-        window.WordCloud = WordCloudMain.default;
-    });
-}).call(this, require || RequireJS.require);
+const WordCloudMain = require('xmodule/assets/word_cloud/src/js/word_cloud_main.js');
+
+window.WordCloud = WordCloudMain.default;
