ac068adc73506ad7b6e1bd049b2c8cfd34b547ad
Josh McLaughlin <josh@opencraft.com>
2020-06-12 01:40:27 -0700

    Hide course Home tab

--- lms/djangoapps/courseware/tabs.py
+++ lms/djangoapps/courseware/tabs.py
@@ -350,7 +350,7 @@ def get_course_tab_list(user, course):
     course_tab_list = []
     must_complete_ee = not user_can_skip_entrance_exam(user, course)
     for tab in xmodule_tab_list:
-        if tab.tab_id == 'discussion':
+        if tab.tab_id in ['discussion', 'info']:
             continue
         if must_complete_ee:
             # Hide all of the tabs except for 'Courseware'
