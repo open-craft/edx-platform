898287c74f89ce155bdc85817a7c4a2ccb3b8435
Kshitij Sobti <kshitij@sobti.in>
2020-07-24 16:48:51 +0530

    Restrict choices for providers Add admin models

---
+++ openedx/core/djangoapps/discussions/admin.py
@@ -0,0 +1,36 @@
+from config_models.admin import KeyedConfigurationModelAdmin
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
+from .models import DiscussionConfig, LearningContextDiscussionConfig
+
+
+class DiscussionConfigAdminModel(KeyedConfigurationModelAdmin):
+    search_fields = ("site", "org", "org_course", "context_key", "slug", "name", "description", "discussion_provider")
+    fieldsets = (
+        (_("Key settings"), {
+            "fields": ("enabled", "slug", "provider",),
+            "description": _("These setting should never be changed once set!"),
+        }),
+        (_("Name and Description"), {
+            "fields": ("name", "description"),
+            "description": _("The name and description can be visible to Course Authors, make them useful.")
+        }),
+        (_("Scope"), {
+            "fields": ("site", "org", "org_course", "context_key"),
+            "description":
+                _("The scope set here will define where all this configuration can be used."
+                  "If nothing is set the config is available globally. If only a site is set"
+                  "then the configuration is only available to learning contexts (courses "
+                  "for instance) in that site. Only of these can be set at a time."
+                  "DO NOT EDIT ONCE SET!")
+        }),
+        (_("Configuration"), {
+            "fields": ("config", "private_config")
+        }),
+    )
+
+
+
+admin.site.register(DiscussionConfig, DiscussionConfigAdminModel)
+admin.site.register(LearningContextDiscussionConfig)
--- openedx/core/djangoapps/discussions/models.py
+++ openedx/core/djangoapps/discussions/models.py
@@ -1,9 +1,18 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from jsonfield import JSONField
 from opaque_keys.edx.django.models import LearningContextKeyField
 
 from openedx.core.djangoapps.config_model_utils.models import CourseAppConfigOptionsModel
+from openedx.core.djangoapps.discussions.discussions_apps import DiscussionAppsPluginManager
+
+
+def _get_provider_choices():
+    return [
+        (provider.name, provider.friendly_name)
+        for provider in DiscussionAppsPluginManager.get_enabled_discussion_apps()
+    ]
 
 
 class DiscussionConfig(CourseAppConfigOptionsModel):
@@ -18,14 +27,15 @@ class DiscussionConfig(CourseAppConfigOptionsModel):
         verbose_name=_("Discussion provider"),
         help_text=_("The discussion tool/provider."),
         null=False,
+        choices=_get_provider_choices(),
     )
     config = JSONField(
-        blank=False,
+        blank=True,
         default={},
         help_text=_("The discussion configuration data that can be user visible."),
     )
     private_config = JSONField(
-        blank=False,
+        blank=True,
         default={},
         help_text=_(
             "The discussion configuration data that contains secret information"
@@ -49,6 +59,12 @@ class LearningContextDiscussionConfig(models.Model):
     config_slug = models.CharField(
         blank=False,
         max_length=100,
-        verbose_name=_("Unique configuration slug"),
-        help_text=_("A unique identifier for this configuration."),
+        verbose_name=_("Configuration slug"),
+        help_text=_("Should be an existing slug from a DiscussionConfig"),
     )
+
+    def __str__(self):
+        return "Using config with slug {config_slug} for {context_key}".format(
+            context_key=self.context_key,
+            config_slug=self.config_slug,
+        )
