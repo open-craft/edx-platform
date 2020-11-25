edd79de9f9594b2d9725b396e47eb71b19c92e38
Nizar Mahmoud <nizarmah@hotmail.com>
2020-11-25 21:42:44 +0300

    Sorts Additional Node Env Variables to keep Webpack Hashes Consistent

--- pavelib/assets.py
+++ pavelib/assets.py
@@ -779,8 +779,17 @@ def webpack(options):
     result = Env.get_django_settings(['STATIC_ROOT', 'WEBPACK_CONFIG_PATH'], "lms", settings=settings)
     static_root_lms, config_path = result
     static_root_cms, = Env.get_django_settings(["STATIC_ROOT"], "cms", settings=settings)
-    additional_node_env_vars = json.dumps(Env.get_django_settings("ADDITIONAL_NODE_ENV_VARS", "cms",
-                                                                  settings=settings).replace("'", '"'))
+    additional_node_env_vars_sorted_json = json.dumps(
+        json.loads(
+            Env.get_django_settings(  # pylint: disable=no-member
+                "ADDITIONAL_NODE_ENV_VARS",
+                "cms",
+                settings=settings
+            ).replace("'", '"')
+        ),
+        sort_keys=True,
+    )
+    additional_node_env_vars = json.dumps(additional_node_env_vars_sorted_json)
     environment = 'NODE_ENV={node_env} STATIC_ROOT_LMS={static_root_lms} STATIC_ROOT_CMS={static_root_cms} \
     ADDITIONAL_NODE_ENV_VARS={additional_node_env_vars}'.format(
         node_env="development" if config_path == 'webpack.dev.config.js' else "production",
