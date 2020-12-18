cbe70d3fc72019fc4708aa4ce1fc7435c030396e
Dylan Grafmyre <dylan@opencraft.com>
2020-12-18 22:49:51 +0000

    prevent a backbone fieldview race condition that can delete user input
    when the user is focused on an input, and the value of the input has
    changed from the model, ignore field updates because these would delete
    user input data.  This data will be submitted normally on finishEditing

--- lms/static/js/views/fields.js
+++ lms/static/js/views/fields.js
@@ -383,7 +383,18 @@
 
             updateValueInField: function() {
                 var value = (_.isUndefined(this.modelValue()) || _.isNull(this.modelValue())) ? '' : this.modelValue();
-                this.$('.u-field-value input').val(value);
+
+                var fieldHasFocus = (document.activeElement === this.$('.u-field-value input')[0]);
+                var fieldChanged = this.fieldValue() !== value;
+                if (fieldHasFocus && fieldChanged) {
+                    // Race conidtion between successive user-changed input
+                    // If user changed input after it was submitted before it was saved,
+                    // do nothing, it will be handled by normal finishEditing hooks.
+                    console.log('TextareaFieldView: race condition ignored')
+                } else {
+                    console.log('TextareaFieldView: no race condition')
+                    this.$('.u-field-value input').val(value);
+                }
             },
 
             saveValue: function() {
@@ -478,7 +489,18 @@
 
             updateValueInField: function() {
                 if (this.editable !== 'never') {
-                    this.$('.u-field-value select').val(this.modelValue() || '');
+                    var value = this.modelValue() || '';
+                    var fieldHasFocus = (document.activeElement === this.$('.u-field-value select')[0]);
+                    var fieldChanged = this.fieldValue() !== value;
+                    if (fieldHasFocus && fieldChanged) {
+                        // Race conidtion between successive user-changed input
+                        // If user changed input after it was submitted before it was saved,
+                        // do nothing, it will be handled by normal finishEditing hooks.
+                        console.log('DropdownFieldView: race condition ignored')
+                    } else {
+                        this.$('.u-field-value select').val(value);
+                        console.log('DropdownFieldView: no race condition')
+                    }
                 }
 
                 var value = this.displayValue(this.modelValue() || '');
