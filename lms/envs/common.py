c6d8d30d436603333f621fdc7710b59edb004025
Farhaan Bukhsh <farhaan@opencraft.com>
2021-02-01 20:15:51 +0530

    Change back the language to en
    Signed-off-by: Farhaan Bukhsh <farhaan@opencraft.com>

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1213,7 +1213,7 @@ MEDIA_URL = '/media/'
 # Locale/Internationalization
 CELERY_TIMEZONE = 'UTC'
 TIME_ZONE = 'UTC'
-LANGUAGE_CODE = 'ar-sa'  # http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en'  # http://www.i18nguy.com/unicode/language-identifiers.html
 # these languages display right to left
 LANGUAGES_BIDI = ("he", "ar", "ar-sa", "fa", "ur", "fa-ir", "rtl")
 
