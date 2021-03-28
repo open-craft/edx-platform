9545bbc515edbbbe4c538b951c036a0f1a6d3961
Shimul Chowdhury <shimul@opencraft.com>
2021-03-28 10:19:22 +0600

    Add missing context platform_name and contact_mailing_address. And pass (#327)
    site configuration to email context.
    
    (cherry picked from commit e942f12adc7d396ab210443ab3e710f53d7caf57)

--- openedx/core/djangoapps/ace_common/template_context.py
+++ openedx/core/djangoapps/ace_common/template_context.py
@@ -42,4 +42,6 @@ def get_base_template_context(site):
             'CONTACT_MAILING_ADDRESS', site=site, site_config_name='contact_mailing_address'),
         'social_media_urls': get_config_value_from_site_or_settings('SOCIAL_MEDIA_FOOTER_URLS', site=site),
         'mobile_store_urls': get_config_value_from_site_or_settings('MOBILE_STORE_URLS', site=site),
+        'logo_url': get_logo_url_for_email(),
+        'site_configuration_values': site_configuration_values,
     }
