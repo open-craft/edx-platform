ba873142a8eb34c39288a393a7f1861c5f35bb77
J. Victor Martins <jvdm@sdf.org>
2021-06-24 17:34:48 -0300

    Make content libraries an LTI 1.3 tool
    Offers blockstore-based content through content libraries  acting as an
    LTI 1.3 tool:
    
    - Content Library support LTI 1.3 launches offering blockstore-based
      content through resource links.
    
    - Content Library support LTI 1.3. AGS, allowing gradebook updates from
      graded assignments.

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -3107,7 +3107,10 @@ INSTALLED_APPS = [
     'lms.djangoapps.bulk_user_retirement',
 
     # Agreements
-    'openedx.core.djangoapps.agreements'
+    'openedx.core.djangoapps.agreements',
+
+    # Content Library LTI 1.3 Support.
+    'pylti1p3.contrib.django.lti1p3_tool_config'
 ]
 
 ######################### CSRF #########################################
