a9c572f3237efd7f4bb560c9832dba45e88b38e8
pkulkark <pooja@opencraft.com>
2021-10-18 12:39:31 +0530

    feat: Make course description editable in Certs
    This makes the default description text that appears under the
    course title editable/configurable for certificates.

--- lms/djangoapps/certificates/views/webview.py
+++ lms/djangoapps/certificates/views/webview.py
@@ -235,20 +235,7 @@ def _update_course_context(request, context, course, course_key, platform_name):
     context['accomplishment_copy_course_name'] = accomplishment_copy_course_name
     course_number = course.display_coursenumber if course.display_coursenumber else course.number
     context['course_number'] = course_number
-    if context['organization_long_name']:
-        # Translators:  This text represents the description of course
-        context['accomplishment_copy_course_description'] = _(u'a course of study offered by {partner_short_name}, '
-                                                              'an online learning initiative of '
-                                                              '{partner_long_name}.').format(
-            partner_short_name=context['organization_short_name'],
-            partner_long_name=context['organization_long_name'],
-            platform_name=platform_name)
-    else:
-        # Translators:  This text represents the description of course
-        context['accomplishment_copy_course_description'] = _('a course of study offered by '
-                                                              '{partner_short_name}.').format(
-            partner_short_name=context['organization_short_name'],
-            platform_name=platform_name)
+    context['accomplishment_copy_course_description'] = context['certificate_data'].get('course_description', '')
 
 
 def _update_social_context(request, context, course, user, user_certificate, platform_name):
