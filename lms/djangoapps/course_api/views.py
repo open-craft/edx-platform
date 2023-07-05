b26683fd2c24fac3d95e66e78b7b79121c87edc8
Yusuf Musleh <yusuf@opencraft.com>
2023-07-05 15:20:41 +0300

    feat: list courses details by keys (#548)
    This adds the ability to get a list of detailed courses based on their
    keys provided in the newly added `keys` query param in the `GET /courses/v1/courses/`
    endpoint.
    
    (cherry picked from commit b14cf0cc76928d3903eafcfda47dbefb44044a7a)

--- lms/djangoapps/course_api/views.py
+++ lms/djangoapps/course_api/views.py
@@ -277,6 +277,10 @@ class CourseListView(DeveloperErrorViewMixin, ListAPIView):
             Notice that Staff users are always granted permission to list any
             course.
 
+        course_keys (optional):
+            If specified, it fetches the `CourseOverview` objects for the
+            the specified course keys
+
     **Returns**
 
         * 200 on success, with a list of course discovery objects as returned
@@ -333,7 +337,8 @@ class CourseListView(DeveloperErrorViewMixin, ListAPIView):
             org=form.cleaned_data['org'],
             filter_=form.cleaned_data['filter_'],
             search_term=form.cleaned_data['search_term'],
-            permissions=form.cleaned_data['permissions']
+            permissions=form.cleaned_data['permissions'],
+            course_keys=form.cleaned_data['course_keys'],
         )
 
 
