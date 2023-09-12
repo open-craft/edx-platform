41637a4d12aa0980522ce485b1fa567f75a2cd53
Navin Karkera <navin@disroot.org>
2023-09-12 15:10:42 +0200

    fix: update migrations from 5816a03 for Palm

--- openedx/core/djangoapps/schedules/migrations/0021_add_due_date_reminder_schedule_config.py
+++ openedx/core/djangoapps/schedules/migrations/0021_add_due_date_reminder_schedule_config.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('schedules', '0021_alter_historicalschedule_options'),
+        ('schedules', '0020_remove_config_rollout_fields'),
     ]
 
     operations = [
