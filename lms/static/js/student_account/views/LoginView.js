open-craft/edx-platform

commit 91f8d361f95b9738adc9e57742a339706aa4e9f6

temp: Add configuration option to redirect to external site when TAP account is unlinked

lms/static/js/student_account/views/LoginView.js
@@ -213,6 +213,7 @@
             saveError: function(error) {
                 var errorCode;
                 var msg;
+                var redirectURL;
                 if (error.status === 0) {
                     msg = gettext('An error has occurred. Check your Internet connection and try again.');
                 } else if (error.status === 500) {
@@ -242,6 +243,7 @@
                 } else if (error.responseJSON !== undefined) {
                     msg = error.responseJSON.value;
                     errorCode = error.responseJSON.error_code;
+                    redirectURL = error.responseJSON.redirect_url;
                 } else {
                     msg = gettext('An unexpected error has occurred.');
                 }
@@ -262,6 +264,9 @@
                     if (!this.hideAuthWarnings) {
                         this.clearFormErrors();
                         this.renderThirdPartyAuthWarning();
+                    }
+                    if (redirectURL){
+                        window.location.href = redirectURL;
                     }
                 } else {
                     this.renderErrors(this.defaultFormErrorsTitle, this.errors);

openedx/core/djangoapps/user_authn/views/login.py
@@ -77,7 +77,7 @@ def _do_third_party_auth(request):
 
     try:
         return pipeline.get_authenticated_user(requested_provider, username, third_party_uid)
-    except USER_MODEL.DoesNotExist:
+    except USER_MODEL.DoesNotExist as err:
         AUDIT_LOG.info(
             "Login failed - user with username {username} has no social auth "
             "with backend_name {backend_name}".format(
@@ -99,7 +99,18 @@ def _do_third_party_auth(request):
             )
         )
 
-        raise AuthFailedError(message, error_code='third-party-auth-with-no-linked-account')  # lint-amnesty, pylint: disable=raise-missing-from
+        # When a user logs in who is authenticated by the OAuth2 but doesn't
+        # have a linked account, redirect them to an external URL where they
+        # can set up their account.
+        # This is a temporary change that will be reverted when the authn MFE is
+        # in use.
+        redirect_url = configuration_helpers.get_value('OC_REDIRECT_ON_TPA_UNLINKED_ACCOUNT', None)
+
+        raise AuthFailedError(
+            message,
+            error_code='third-party-auth-with-no-linked-account',
+            redirect_url=redirect_url
+        ) from err
 
 
 def _get_user_by_email(email):

commit 9ade4eac447f5b90d271b26cfee1c852c2e3c559

fix: prevent redirects to /undefined after saml auth (#669)
When a user authenticates using SAML, but their accounts aren't linked,
they land on the login page with the message to link their accounts. If the
`OC_REDIRECT_ON_TPA_UNLINKED_ACCOUNT` value is not set, they are redirected to
`/undefined`. This commit checks that the `redirectURL` is a valid non-empty
string before performing the redirect.

The bug was introduced in 91f8d36, so this can be safely dropped once that
temporary commit is removed from our branches.

Internal-ref: https://tasks.opencraft.com/browse/BB-9010

lms/static/js/student_account/views/LoginView.js
@@ -267,7 +267,9 @@
                         this.clearFormErrors();
                         this.renderThirdPartyAuthWarning();
                     }
-                    window.location.href = redirectURL;
+                    if (typeof redirectURL === "string" && redirectURL.length) {
+                        window.location.href = redirectURL;
+                    }
                 } else {
                     this.renderErrors(this.defaultFormErrorsTitle, this.errors);
                 }